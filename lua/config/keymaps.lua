8292b5d12382b686855aa81dd9d2c7f234ade984
Jack McRobbie <mcrobbiej@unimelb.edu.au>
2024-06-21 16:34:41 +1000

    adds gopening files in preview, Pdfs etc

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- set tab to go to next buffer
+vim.api.nvim_set_keymap("n", "<Tab>", ":bnext<CR>", { noremap = true, silent = true })
+--set shift tab to go to next window (split)
+vim.api.nvim_set_keymap("n", "<S-Tab>", ":wincmd w<CR>", { noremap = true, silent = true })
+
+vim.api.nvim_create_user_command("OpenInPreview", function()
+  vim.cmd("silent !open -a Preview.app " .. vim.fn.shellescape(vim.fn.expand("%:p")))
+end, {})
+vim.api.nvim_set_keymap("n", "<leader>P", ":silent !open -a Preview.app %<CR>", { noremap = true, silent = true })
